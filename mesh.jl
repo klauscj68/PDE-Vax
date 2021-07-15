f0fbe4aebc2def0d2f4a866e96524f9c4362f0a5
Colin Klaus <colin.js.klaus@gmail.com>
2021-07-14 22:51:51 -0400

    Changed meshing routines so it lists rectangles horizontally then vertically, while before it had been reversed.  Added new routines for evaluating linear interpolant splines and determining membership of query points inside triangles although these routines have not been tested yet. Also added environment files.
    On branch master
    Changes to be committed:
    	new file:   Manifest.toml
    	new file:   Project.toml
    	modified:   mesh.jl

--- mesh.jl
+++ mesh.jl
@@ -79,23 +79,25 @@ struct mesh
 	urg::Vector{Float64}
 	trg::Vector{Float64}
 	ntics::Int64
+	uaxis::Vector{Float64}
+	taxis::Vector{Float64}
 
 	function mesh(urg::Vector{Float64},trg::Vector{Float64},ntics::Int64)
 		# Generate u-t axes tics
-		uaxis = LinRange(urg[1],urg[2],ntics); 
-		taxis = LinRange(trg[1],trg[2],ntics);
+		uaxis = convert(Vector,LinRange(urg[1],urg[2],ntics));
+		taxis = convert(Vector,LinRange(trg[1],trg[2],ntics));
 
 		## Build vector of nodes
 		# Generate node coordinates
-		ndu = repeat(reshape(uaxis,1,ntics),outer=(ntics,1));
-		ndt = repeat(reshape(taxis,ntics,1),outer=(1,ntics));
+		ndu = repeat(reshape(uaxis,ntics,1),outer=(1,ntics));
+		ndt = repeat(reshape(taxis,1,ntics),outer=(ntics,1));
 
 		ndpts = convert(Matrix,transpose([ndu[:] ndt[:]]));
 
 		# Generate nodes by tic index pairings
 		idram = 1:ntics;
-		ndidu = repeat(reshape(idram,1,ntics),outer=(ntics,1));
-		ndidt = repeat(reshape(idram,ntics,1),outer=(1,ntics));
+		ndidu = repeat(reshape(idram,ntics,1),outer=(1,ntics));
+		ndidt = repeat(reshape(idram,1,ntics),outer=(ntics,1));
 
 		ndid = convert(Matrix,transpose([ndidu[:] ndidt[:]]));
 
@@ -112,16 +114,16 @@ struct mesh
 		for k=1:nrects
 			# Extract rectangular indices of bottom left node
 			# (matching ndid[:,⋅] in [i;j] form)
-			#  k = (i-1)*(ntics-1) + j for 1<=i,j<=ntics-1
-			#  => k-1 = i¯*(ntics-1)+j¯ for 0<=i¯,j¯<ntics-1
-			i = 1 + Int64(floor((k-1)/(ntics-1)));
-			j = 1 + (k-1-(i-1)*(ntics-1));
+			#  k = (j-1)*(ntics-1) + i for 1<=i,j<=ntics-1
+			#  => k-1 = j¯*(ntics-1)+i¯ for 0<=i¯,j¯<ntics-1
+			j = 1 + Int64(floor((k-1)/(ntics-1)));
+			i = 1 + (k-1-(j-1)*(ntics-1));
 			
 			# Compute (i,j) position in ntics^2 ordering
 			# (ie compute ⋅)
-			#  q = (i-1)*ntics + j
+			#  q = (j-1)*ntics + i
 			#  counter-clockwise tour of rectangle from bottom left
-			q = [(i-1),i,i,i-1]*ntics + [j,j,j+1,j+1];
+			q = [i,i+1,i+1,i] + [j-1,j-1,j,j]*ntics;
 
 			# Store in rects list
 			rects[:,k] = q;
@@ -143,6 +145,107 @@ struct mesh
 		end
 		elms = tris[:];
 
-		return new(nd,elms,urg,trg,ntics)
+		return new(nd,elms,urg,trg,ntics,uaxis,taxis)
 	end
+end
+
+# mbrtri
+"""
+Test whether given point belongs to given triangle
+t:: triangle checking for membership
+pt:: point checking to see if in triangle
+flagbx:: optional Boolean saying if instead of exact membership
+         test to see if can rule out membership based on simple bounding
+	 box check
+"""
+function mbrtri(t::tri,pt::Vector{Float64};flagbx::Bool=false)
+	if !flagbx
+		# Compute barycentric coordinates
+		b = t.Btr*[1.;pt];
+		nneg = sum(b.<0);
+
+		return nneg == 0
+	else
+		# Test if belongs to bounding box
+		return (t.urg[1] <= pt[1])&(t.urg[1] >= pt[1])&
+		        (t.urg[2] <= pt[2])&(t.urg[2] >= pt[2])
+	end
+end
+
+"""
+Find the first triangle containing each query point 
+(-1 means not found)
+m:: mesh over which testing for membership
+q:: 2 x nq array that stores query points
+"""
+function mbrtri(m::mesh,q::Matrix{Float64})
+	nq = size(q)[2];
+	nelms = length(m.elms);
+	
+	# Find triangles containing point
+	pos = repeat([-1],outer=(nq,));
+	for k=1:nq*nelms
+		# k = (i-1)*nq+j for 1<=i<=nq and 1<=j<=nelms
+		#  => k-1 = (i-1)*nq+(j-1)
+		i = 1 + Int64(floor((k-1)/nq));
+		j = 1 + (k-1-(i-1)*nq);
+		
+		if mbrtri(m.elms[j],q[:,i];flagbx=true)
+			pos[i] = mbrtri(m.elms[j],q[:,i]) ? j : -1;
+			if pos[i] != -1
+				continue
+			end
+		end
+	end
+	return pos
+end
+""" 
+Same as other mbrtri but returns an Int64 rather than array since working
+with single point
+"""
+function mbrtri(m::mesh,q::Vector{Float64})
+	qram = reshape(q,outer=(2,1));
+	pos = mbrtri(m,qram)
+
+	return pos[1]
+end
+
+# eval
+"""
+Evaluate a 1d linear interpolant spline at given query points
+c:: gives nodal coefficient values in the order of the nodes of m
+m:: mesh over which evaluating interpolant
+q:: 2 x nq array that stores query points
+"""
+function eval(c::Vector{Float64},m::mesh,q::Matrix{Float64})
+	nq = size(q)[2];
+
+	# Find triangles containing query points
+	pos = mbrtri(m,q);
+
+	# Evaluate spline at queries
+	val = Vector{Float64}(undef,nq);
+	for k=1:nq
+		if pos[k] == -1
+			val[k] = NaN;
+			continue
+		end
+
+		tri = m.elms[pos[k]];
+		nds = tri.ndid;
+		b = tri.Btr*[1;q[:,k]];
+		cval = [c[nds[1]],c[nds[2]],c[nds[3]]];
+
+		val[k] = sum(cval.*b);
+	end
+end
+"""
+Same as other eval except because working with singple point returns 
+Float64 not array
+"""
+function eval(c::Vector{Float64},m::mesh,q::Vector{Float64})
+	qram = reshape(q,outer=(2,1));
+	val = eval(c,m,qram);
+
+	return val[1]
 end
